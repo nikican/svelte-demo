4f34d26efc373399ce34c0500850e4d361e7a4c7
Nikica Novakovic <novakovicnikica@gmail.com>
2020-06-04 23:23:28 +0200

    Showcase importing and exporting

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+  import Face from "./Face.svelte";
+
+  let say = "hi";
+  setTimeout(() => {
+    say = "bye";
+  }, 1000);
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  div {
+    color: red;
+  }
 </style>
+
+<div>Say: {say}</div>
+
+<Face size="4" />
+<Face size="10" />
+<Face />
---
+++ src/Face.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let size = 1;
+</script>
+
+<div style="font-size: {size}em">=)</div>
