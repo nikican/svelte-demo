4f1869f5bf7c1e884edde8ef7497e9079ce02902
Nikica Novakovic <novakovicnikica@gmail.com>
2020-06-04 23:39:45 +0200

    Challenge 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   import Face from "./Face.svelte";
   import Container from "./Container.svelte";
+  import Header from "./Header.svelte";
 
-  let say = false;
+  let showHeader = false;
   setTimeout(() => {
-    say = "bye";
+    showHeader = true;
   }, 1000);
 </script>
 
@@ -12,13 +13,21 @@
   div {
     color: red;
   }
+  :global(*) {
+    box-sizing: border-box;
+  }
+  :global(body, html) {
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
+  }
 </style>
 
-<Container>
-  {#if say}
-    <div>Hi!</div>
-  {:else if !say}not saying anything..{/if}
-  {#each [2, 1, 0] as index}
-    <Face {index} />
-  {/each}
-</Container>
+{#if showHeader}
+  <Header />
+{/if}
+<Container />
+
+<!-- Challenge 2 - 
+1. use an if statement and settimeout to make Header appear after 1 second (1000 milliseconds)
+2. inside the header, make the face+sveltelogo repeat with #each  -->
